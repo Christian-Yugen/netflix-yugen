c707d2bf0b6f8a89b91fa59ac40816528fbbce3a
Christian Yugen <117937220+Christian-Yugen@users.noreply.github.com>
2023-05-05 23:34:27 -0300

    Correção de codigo que não colocava mascara no password

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -2,19 +2,21 @@ import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 
-export default asyn funcion handler(req: NextApiRequest, res: NextApiResponse) {
+export default asyn function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         return res.status(405).end();
       }
   
+    try{
+
       const { email, name, password } = req.body;
-  
+
       const existingUser = await prismadb.user.findUnique({
         where: {
-          email
+          email,
         }
       })
-  
+    
       if (existingUser) {
         return res.status(422).json({ error: 'Email taken' });
       }
@@ -32,7 +34,9 @@ export default asyn funcion handler(req: NextApiRequest, res: NextApiResponse) {
       })
   
       return res.status(200).json(user);
+
     } catch (error) {
+      console.log(error);
       return res.status(400).json({ error: `Something went wrong: ${error}` });
     }
   }
