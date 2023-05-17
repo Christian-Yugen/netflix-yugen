2e8cf7dcca2baf6b10b3fdf374f17f8dbd06fd8d
Christian Yugen <117937220+Christian-Yugen@users.noreply.github.com>
2023-05-16 22:58:34 -0300

    inclusão de botão logout e configurações de index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import { signOut } from "next-auth/react"
+
+
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl text-green-500">Hello Netflix Yugen</h1>
+      <button onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
